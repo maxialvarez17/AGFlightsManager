6379cddf6bcdfc00e2bb390fdadc658952f5bf47
maxialvarez17 <maximilianoalvarez17@gmail.com>
2018-07-03 12:18:56 +0200

    Excercise 1 - Flights Manager - Full app

---
+++ FlightsManager/FlightsManager.Contracts/Data/Repositories/IReadOnlyRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightsManager.Contracts.Data.Repositories
+{
+    public interface IReadOnlyRepository<TEntity> where TEntity : class
+    {
+        ICollection<TEntity> GetAll();
+
+        TEntity Get(int id);
+    }
+}
---
+++ FlightsManager/FlightsManager.Contracts/Data/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightsManager.Contracts.Data.Repositories
+{
+    public interface IRepository<TEntity> : IReadOnlyRepository<TEntity> where TEntity : class
+    {
+        void Insert(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(int id);
+    }
+}
